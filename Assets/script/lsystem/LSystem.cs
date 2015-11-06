97189d66a13d01eecd37a465292527d93b7e8247
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2015-11-06 11:33:39 +0100

    Add random ranges for L-System

--- Assets/script/lsystem/LSystem.cs
+++ Assets/script/lsystem/LSystem.cs
@@ -7,23 +7,39 @@ public class LSystem : MonoBehaviour {
 	public MeshFilter leaves;
 	List<LSElement> str;
 
+	public int seed = 0;
+	public float smoothSeed = 0;
 	[MinMaxRange(5,200)]
-	public Vector2 length0 = new Vector2(100,100);
-	public float w0 = 20;
-	public float alpha1 = 30;
-	public float alpha2 = -30;
-	public float phi1 = 137;
-	public float phi2 = 137;
-	public float r1 = 0.8f;
-	public float r2 = 0.8f;
-	public float q = 0.5f;
-	public float e = 0.5f;
-	public float smin = 0;
+	public Vector2 initialLength = new Vector2(100,100);
+	[MinMaxRange(5,300)]
+	public Vector2 initialWidth = new Vector2(20,20);
+	[MinMaxRange(-180,180)]
+	public Vector2 alpha1 = Vector2.one * 30;
+	[MinMaxRange(-180,180)]
+	public Vector2 alpha2 = Vector2.one * -30;
+	[MinMaxRange(-180,180)]
+	public Vector2 phi1 = Vector2.one * 137;
+	[MinMaxRange(-180,180)]
+	public Vector2 phi2 = Vector2.one * 137;
+	[MinMaxRange(0.1f,1.0f)]
+	public Vector2 r1 = Vector2.one * 0.8f;
+	[MinMaxRange(0.1f,1.0f)]
+	public Vector2 r2 = Vector2.one * 0.8f;
+	[MinMaxRange(0.1f,1.0f)]
+	public Vector2 q = Vector2.one * 0.5f;
+	[MinMaxRange(0.1f,1.0f)]
+	public Vector2 e = Vector2.one * 0.5f;
+	[MinMaxRange(0,2)]
+	public Vector2 smin = Vector2.one * 0;
 	public int iter = 8;
-	public float leafMid = 0.5f;
-	public float leafLength = 2;
-	public float leafWidth = 2;
-	public float leafRotate = 0;
+	[MinMaxRange(0,1)]
+	public Vector2 leafMid = Vector2.one * 0.5f;
+	[MinMaxRange(0.01f,5)]
+	public Vector2 leafLength = Vector2.one * 2;
+	[MinMaxRange(0.01f,5)]
+	public Vector2 leafWidth = Vector2.one * 2;
+	[MinMaxRange(-100,100)]
+	public Vector2 leafRotate = Vector2.one * 0;
 	List<Vector3> vertices = new List<Vector3>();
 	List<int> indices = new List<int>();
 
@@ -31,13 +47,19 @@ public class LSystem : MonoBehaviour {
 	List<int> indicesLeaf = new List<int>();
 
 	void Start(){
+		seed = GetInstanceID ();
 		str = new List<LSElement>();
 		UpdateTree ();
 	}
 
-	void UpdateTree(){
+	public Mesh[] Build(){
+		ResetRandom (seed);
 		ExpandRules();
-		var meshes = Interpret();
+		return Interpret();
+	}
+
+	void UpdateTree(){
+		var meshes = Build ();
 		var meshFilters = new MeshFilter[]{branches,leaves};
 		for (int i = 0; i < meshes.Length; i++) {
 			meshes[i].RecalculateNormals ();
@@ -59,17 +81,23 @@ public class LSystem : MonoBehaviour {
 #endif
 	}
 
-	float Eval(Vector2 v){
-		return Random.Range (v.x, v.y);
+	public static void ResetRandom(int seed1){
+		Random.seed = seed1;
+	}
+
+	public float Eval(Vector2 v){
+		float val = Runevision.Structures.SimplexNoise.Noise(new Vector3(Random.value*100000,smoothSeed));
+		val = val * 0.5f + 0.5f;
+		return Mathf.Lerp(v.x, v.y, val);
 	}
 
 	void ExpandRules(){
 		str.Clear();
-		str.Add(new LSElement(LSElement.LSSymbol.APEX, Eval(length0), w0));
+		str.Add(new LSElement(LSElement.LSSymbol.APEX, Eval(initialLength), Eval(initialWidth)));
 
 		// string debug = "Expanding "+str[0]+" to ";
 
-		Rule r = new Rule(alpha1, alpha2, phi1, phi2, r1, r2, q, e, smin);
+		Rule r = new Rule(alpha1, alpha2, phi1, phi2, r1, r2, q, e, smin, this);
 		for (int i=0;i<iter;i++){
 			List<LSElement> outList = new List<LSElement>();
 			bool lastIter = (i == iter-1);
@@ -85,13 +113,17 @@ public class LSystem : MonoBehaviour {
 	}
 
 	void AddLeaf(Matrix4x4 m, float len, float width){
-		len *= leafLength;
-		width *= leafWidth;
+		len *= Eval(leafLength);
+		width *= Eval(leafWidth);
 
+		float leafRot = Eval (leafRotate);
+		float w = width;
+		float l = len;
+		float mid = l * Eval(leafMid);
 		Vector3 p0 = m.MultiplyPoint(new Vector3(0, 0, 0));
-		Vector3 p1 = m.MultiplyPoint(Quaternion.Euler(0,0,leafRotate) * new Vector3(0, width, len*leafMid));
-		Vector3 p2 = m.MultiplyPoint(new Vector3(0, 0, len));
-		Vector3 p3 = m.MultiplyPoint(Quaternion.Euler(0,0,-leafRotate) * new Vector3(0, -width, len*leafMid));
+		Vector3 p1 = m.MultiplyPoint(Quaternion.Euler(0,0,leafRot) * new Vector3(0, w, mid));
+		Vector3 p2 = m.MultiplyPoint(new Vector3(0, 0, l));
+		Vector3 p3 = m.MultiplyPoint(Quaternion.Euler(0,0,-leafRot) * new Vector3(0, -w, mid));
 		verticesLeaf.Add (p0);
 		verticesLeaf.Add (p1);
 		verticesLeaf.Add (p2);
@@ -132,7 +164,7 @@ public class LSystem : MonoBehaviour {
 	}
 
 	public Mesh[] Interpret(){
-		Turtle turtle = new Turtle(w0);
+		Turtle turtle = new Turtle(Eval(initialWidth));
 
 		foreach (var elem in str){
 			switch (elem.symbol){
@@ -140,8 +172,9 @@ public class LSystem : MonoBehaviour {
 				AddLeaf (turtle.Peek().M,elem.data [0], elem.data [1]); 
 				break;
 			case LSElement.LSSymbol.DRAW:
-				AddCone(turtle.Peek().M, elem.data[0], turtle.GetWidth(), turtle.GetWidth() * elem.data[1]);
-				turtle.Move(elem.data[0]);
+				float movDist = elem.data [0];
+				AddCone(turtle.Peek().M, movDist, turtle.GetWidth(), turtle.GetWidth() * elem.data[1]);
+				turtle.Move(movDist);
 				break;
 			case LSElement.LSSymbol.TURN:
 				turtle.Turn(elem.data[0]);
@@ -176,128 +209,128 @@ public class LSystem : MonoBehaviour {
 
 	void OnGUI(){
 		if (GUI.Button(new Rect(0,0,50,30), "Fig.a")){
-			r1 = 0.75f;
-			r2 = 0.77f;
-			alpha1 = 35;
-			alpha2 = -35;
-			phi1 = 0;
-			phi2 = 0;
-			w0 = 30;
-			q = 0.5f;
-			e = 0.4f;
-			smin = 0;
+			r1 = Vector2.one*0.75f;
+			r2 = Vector2.one*0.77f;
+			alpha1 = Vector2.one*35;
+			alpha2 = Vector2.one*-35;
+			phi1 = Vector2.one*0;
+			phi2 = Vector2.one*0;
+			initialWidth = new Vector2(30,30);
+			q = Vector2.one*0.5f;
+			e = Vector2.one*0.4f;
+			smin = Vector2.one*0;
 			iter = 10;
 			UpdateTree ();
 		}
 		if (GUI.Button(new Rect(50,0,50,30), "Fig.b")){
-			r1 = 0.65f;
-			r2 = 0.71f;
-			alpha1 = 27;
-			alpha2 = -68;
-			phi1 = 0;
-			phi2 = 0;
-			w0 = 20;
-			q = 0.53f;
-			e = 0.5f;
-			smin = 1.7f;
+			r1 = Vector2.one*0.65f;
+			r2 = Vector2.one*0.71f;
+			alpha1 = Vector2.one*27;
+			alpha2 = Vector2.one*-68;
+			phi1 = Vector2.one*0;
+			phi2 = Vector2.one*0;
+			initialWidth = new Vector2(20,20);
+			q = Vector2.one*0.53f;
+			e = Vector2.one*0.5f;
+			smin = Vector2.one*1.7f;
 			iter = 10;
 			UpdateTree ();
 		}
 		if (GUI.Button(new Rect(100,0,50,30), "Fig.c")){
-			r1 = 0.5f;
-			r2 = 0.85f;
-			alpha1 = 25;
-			alpha2 = -15;
-			phi1 = 180;
-			phi2 = 0;
-			w0 = 20;
-			q = 0.45f;
-			e = 0.5f;
-			smin = 0.5f;
+			r1 = Vector2.one*0.5f;
+			r2 = Vector2.one*0.85f;
+			alpha1 = Vector2.one*25;
+			alpha2 = Vector2.one*-15;
+			phi1 = Vector2.one*180;
+			phi2 = Vector2.one*0;
+			initialWidth = new Vector2(20,20);
+			q = Vector2.one*0.45f;
+			e = Vector2.one*0.5f;
+			smin = Vector2.one*0.5f;
 			iter = 9;
 			UpdateTree ();
 		}
 		if (GUI.Button(new Rect(150,0,50,30), "Fig.d")){
-			r1 = 0.6f;
-			r2 = 0.85f;
-			alpha1 = 25;
-			alpha2 = -15;
-			phi1 = 180;
-			phi2 = 180;
-			w0 = 20;
-			q = 0.45f;
-			e = 0.5f;
-			smin = 0.0f;
+			r1 = Vector2.one*0.6f;
+			r2 = Vector2.one*0.85f;
+			alpha1 = Vector2.one*25;
+			alpha2 = Vector2.one*-15;
+			phi1 = Vector2.one*180;
+			phi2 = Vector2.one*180;
+			initialWidth = new Vector2(20,20);
+			q = Vector2.one*0.45f;
+			e = Vector2.one*0.5f;
+			smin = Vector2.one*0.0f;
 			iter = 10;
 			UpdateTree ();
 		}
 		if (GUI.Button(new Rect(200,0,50,30), "Fig.e")){
-			r1 = 0.58f;
-			r2 = 0.83f;
-			alpha1 = 30;
-			alpha2 = 15;
-			phi1 = 0;
-			phi2 = 180;
-			w0 = 20;
-			q = 0.40f;
-			e = 0.5f;
-			smin = 1.0f;
+			r1 = Vector2.one*0.58f;
+			r2 = Vector2.one*0.83f;
+			alpha1 = Vector2.one*30;
+			alpha2 = Vector2.one*15;
+			phi1 = Vector2.one*0;
+			phi2 = Vector2.one*180;
+			initialWidth = new Vector2(20,20);
+			q = Vector2.one*0.40f;
+			e = Vector2.one*0.5f;
+			smin = Vector2.one*1.0f;
 			iter = 10;
 			UpdateTree ();
 		}
 		if (GUI.Button(new Rect(250,0,50,30), "Fig.f")){
-			r1 = 0.92f;
-			r2 = 0.37f;
-			alpha1 = 0;
-			alpha2 = 60;
-			phi1 = 180;
-			phi2 = 0;
-			w0 = 2;
-			q = 0.50f;
-			e = 0.0f;
-			smin = 0.5f;
+			r1 = Vector2.one*0.92f;
+			r2 = Vector2.one*0.37f;
+			alpha1 = Vector2.one*0;
+			alpha2 = Vector2.one*60;
+			phi1 = Vector2.one*180;
+			phi2 = Vector2.one*0;
+			initialWidth = new Vector2(2,2);
+			q = Vector2.one*0.50f;
+			e = Vector2.one*0.0f;
+			smin = Vector2.one*0.5f;
 			iter = 12;
 			UpdateTree ();
 		}
 		if (GUI.Button(new Rect(300,0,50,30), "Fig.g")){
-			r1 = 0.80f;
-			r2 = 0.80f;
-			alpha1 = 30;
-			alpha2 = -30;
-			phi1 = 137;
-			phi2 = 137;
-			w0 = 30;
-			q = 0.50f;
-			e = 0.5f;
-			smin = 0.0f;
+			r1 = Vector2.one*0.80f;
+			r2 = Vector2.one*0.80f;
+			alpha1 = Vector2.one*30;
+			alpha2 = Vector2.one*-30;
+			phi1 = Vector2.one*137;
+			phi2 = Vector2.one*137;
+			initialWidth = new Vector2(30,30);
+			q = Vector2.one*0.50f;
+			e = Vector2.one*0.5f;
+			smin = Vector2.one*0.0f;
 			iter = 10;
 			UpdateTree ();
 		}
 		if (GUI.Button(new Rect(350,0,50,30), "Fig.h")){
-			r1 = 0.95f;
-			r2 = 0.75f;
-			alpha1 = 5;
-			alpha2 = -30;
-			phi1 = -90;
-			phi2 = 90;
-			w0 = 40;
-			q = 0.60f;
-			e = 0.45f;
-			smin = 25.0f;
+			r1 = Vector2.one*0.95f;
+			r2 = Vector2.one*0.75f;
+			alpha1 = Vector2.one*5;
+			alpha2 = Vector2.one*-30;
+			phi1 = Vector2.one*-90;
+			phi2 = Vector2.one*90;
+			initialWidth = new Vector2(40,40);
+			q = Vector2.one*0.60f;
+			e = Vector2.one*0.45f;
+			smin = Vector2.one*25.0f;
 			iter = 12;
 			UpdateTree ();
 		}
 		if (GUI.Button(new Rect(400,0,50,30), "Fig.i")){
-			r1 = 0.55f;
-			r2 = 0.95f;
-			alpha1 = -5;
-			alpha2 = 30;
-			phi1 = 137;
-			phi2 = 137;
-			w0 = 5;
-			q = 0.40f;
-			e = 0.00f;
-			smin = 5.0f;
+			r1 = Vector2.one*0.55f;
+			r2 = Vector2.one*0.95f;
+			alpha1 = Vector2.one*-5;
+			alpha2 = Vector2.one*30;
+			phi1 = Vector2.one*137;
+			phi2 = Vector2.one*137;
+			initialWidth = new Vector2(5,5);
+			q = Vector2.one*0.40f;
+			e = Vector2.one*0.00f;
+			smin = Vector2.one*5.0f;
 			iter = 12;
 			UpdateTree ();
 		}
--- Assets/script/lsystem/Rule.cs
+++ Assets/script/lsystem/Rule.cs
@@ -3,18 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Rule {
-	public float alpha1;
-	public float alpha2;
-	public float phi1;
-	public float phi2;
-	public float r1;
-	public float r2;
-	public float q;
-	public float e;
-	public float smin;
+	public Vector2 alpha1;
+	public Vector2 alpha2;
+	public Vector2 phi1;
+	public Vector2 phi2;
+	public Vector2 r1;
+	public Vector2 r2;
+	public Vector2 q;
+	public Vector2 e;
+	public Vector2 smin;
+	LSystem lsys;
 
 
-	public Rule (float alpha1, float alpha2, float phi1, float phi2, float r1, float r2, float q, float e, float smin)
+	public Rule (Vector2 alpha1, Vector2 alpha2, Vector2 phi1, Vector2 phi2, Vector2 r1, Vector2 r2, Vector2 q, Vector2 e, Vector2 smin, LSystem lsys)
 	{
 		this.alpha1 = alpha1;
 		this.alpha2 = alpha2;
@@ -25,6 +26,7 @@ public class Rule {
 		this.q = q;
 		this.e = e;
 		this.smin = smin;
+		this.lsys = lsys;
 	}
 	
 
@@ -32,32 +34,30 @@ public class Rule {
 		if (elem.symbol == LSElement.LSSymbol.APEX){
 			float length = elem.data[0];
 			float width = elem.data[1];
-			if (length >= smin) {
-#if SOLUTION_CODE
+			if (length >= lsys.Eval(smin)) {
 				outList.Add (new LSElement (LSElement.LSSymbol.WIDTH, width));
+				float e = lsys.Eval (this.e);
+				float q = lsys.Eval (this.q);
+				float r1 = lsys.Eval (this.r1);
+				float r2 = lsys.Eval (this.r2);
 				float endScale = Mathf.Max (Mathf.Pow (q, e), Mathf.Pow (1.0f - q, e)); // extension to give a better looking result
 				outList.Add (new LSElement (LSElement.LSSymbol.DRAW, length, endScale));
 				outList.Add (new LSElement (LSElement.LSSymbol.PUSH_STATE));
-				outList.Add (new LSElement (LSElement.LSSymbol.TURN, alpha1));
-				outList.Add (new LSElement (LSElement.LSSymbol.ROLL, phi1));
+				outList.Add (new LSElement (LSElement.LSSymbol.TURN, lsys.Eval(alpha1)));
+				outList.Add (new LSElement (LSElement.LSSymbol.ROLL, lsys.Eval(phi1)));
 				outList.Add (new LSElement (LSElement.LSSymbol.APEX, length * r1, width * Mathf.Pow (q, e)));
 				if (lastIter){
 					outList.Add (new LSElement (LSElement.LSSymbol.LEAF, length, width));
 				}
 				outList.Add (new LSElement (LSElement.LSSymbol.POP_STATE));
 				outList.Add (new LSElement (LSElement.LSSymbol.PUSH_STATE));
-				outList.Add (new LSElement (LSElement.LSSymbol.TURN, alpha2));
-				outList.Add (new LSElement (LSElement.LSSymbol.ROLL, phi2));
+				outList.Add (new LSElement (LSElement.LSSymbol.TURN, lsys.Eval(alpha2)));
+				outList.Add (new LSElement (LSElement.LSSymbol.ROLL, lsys.Eval(phi2)));
 				outList.Add (new LSElement (LSElement.LSSymbol.APEX, length * r2, width * Mathf.Pow (1.0f - q, e)));
 				if (lastIter){
 					outList.Add (new LSElement (LSElement.LSSymbol.LEAF, length, width));
 				}
 				outList.Add (new LSElement (LSElement.LSSymbol.POP_STATE));
-#else
-				outList.Add(new LSElement(LSElement.LSSymbol.DRAW, length, 1));
-				outList.Add(new LSElement(LSElement.LSSymbol.A, length*r1, width*Mathf.Pow(q,e)));
-#endif
-
 			} else {
 				outList.Add (new LSElement (LSElement.LSSymbol.LEAF, length, width));
 			}
